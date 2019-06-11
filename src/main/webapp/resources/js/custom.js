0a7a723d46103c6e9d957106fa9343c244c81310
upnaidu <u.pentayya@gmail.com>
2019-06-12 00:34:16 +0530

    un used files deleted and test cases added

--- src/main/webapp/resources/js/custom.js
+++
@@ -1,8 +0,0 @@
-var list_highlight_page=0;
-function list_removehighlight(){
-	
-		for(var i=1;i<=9;i++){
-			$("#active_"+i).removeClass("active");
-		}
-		$("#active_"+list_highlight_page).addClass("active");
-}
--- src/test/java/com/app/WebControllerTest.java
+++ src/test/java/com/app/WebControllerTest.java
@@ -6,25 +6,54 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.logging.Logger;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.app.controller.WebController;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(WebController.class)
-public class SamplewebApplicationTests {
+@SpringBootTest
+public class WebControllerTest {
+
+	public static final Logger logger = Logger.getLogger(WebControllerTest.class.getName());
+
+	@InjectMocks
+	private WebController webController;
 
 	@Autowired
+	private WebApplicationContext wac;
+
 	private MockMvc mockMvc;
 
+	@Before
+	public void setup() {
+
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+		MockitoAnnotations.initMocks(this);
+
+	}
+
 	@Test
-	public void lginTests() throws Exception {
+	public void testLogin() throws Exception {
 		this.mockMvc.perform(get("/login")).andDo(print()).andExpect(status().isOk()).andExpect(view().name("login"))
 				.andExpect(forwardedUrl("/WEB-INF/views/login.jsp"));
 	}
+
+	@After
+	public void tearDown() {
+		mockMvc = null;
+	}
+
 }
